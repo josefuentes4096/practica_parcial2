37ee23e1eeba3ddf792fdd27821e3cf95c2c80a8
Jose Fuentes <josefuentes4096@gmail.com>
2025-06-12 20:10:48 -0300

    Punto 2)

---
+++ ejercicio1.cpp
@@ -0,0 +1,27 @@
+/**
+ *      1) Se desea conocer qué alumnos cursan Programación o Laboratorio. 
+ * Para hacerlo se dispone de la lista de alumnos de cada materia, y de cada 
+ * alumno se conoce: Legajo (entero de 8 dígitos) y Nombre.
+ * Realizar codificación del procedimiento quienesPrograman que reciba los 
+ * vectores de ambas materias y complete un tercer vector.
+ * 
+ * Consideraciones
+ *     • Los vectores recibidos están ordenados en forma ascendente por 
+ *       legajo, y así se debe generar el tercero.
+ *     • El vector generado no debe tener alumnos repetidos, es decir si 
+ *       Juan cursa ambas materias en el tercer vector solo debe aparecer una vez.
+ *     • Cada alumno está como máximo solamente una vez en cada vector
+ *     • No preocuparse por quién dimensiona el tercer vector, ni su cantidad 
+ *       de elementos, se recibe por parámetro y se usa.
+ * 
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
