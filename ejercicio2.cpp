efc21b4fe6219de237d22768c44087d2c978c8e6
Jose Fuentes <josefuentes4096@gmail.com>
2025-06-12 21:55:48 -0300

    Enunciado ejercicio 2

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -1,6 +1,16 @@
 /**
- *      1) 
+ *    2)  Utilizando el vector de alumnos que programan se desea realizar un listado por consola con 
+ * la información de contacto. Para esto la facultad dispone de un conjunto de datos de Alumnos ordenados 
+ * por Legajo en forma ascendente,  que contiene:
+ *     • Legajo (entero de 8 dígitos)
+ *     • Apellido y Nombre (texto)
+ *     • Email (texto)
+ *     • Telefono (entero)
  * 
+ * Realizar el diagrama del procedimiento o función que reciba el vector de quienes programan y el vector 
+ * de datos de alumnos e imprima el listado ordenado por apellido y nombre:
+ * APELLIDO Y NOMBRE		  LEGAJO		EMAIL		TELEFONO
+ *  
 */
 
 #include <iostream>
